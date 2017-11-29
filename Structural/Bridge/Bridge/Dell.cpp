ea6668bae4c359a68c9cc17b5dd262012670c23c
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-11-29 13:40:26 +0200

    new definition

--- Structural/Bridge/Bridge/Dell.cpp
+++ Structural/Bridge/Bridge/Dell.cpp
@@ -26,6 +26,6 @@ void Dell::setDisplay()
 
 void Dell::setVideocard()
 {
-	vcard->setVideocard();	
-	vcard->runVideocard();
+	vcard->setVideocard();	// set matched card
+	vcard->runVideocard();	// and run
 }
